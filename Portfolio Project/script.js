9e3c3995d6e2a245b03d62317b47e1243a5dd739
KyloAmari <136878942+KyloAmari@users.noreply.github.com>
2026-03-25 00:10:42 +0800

    Delete Portfolio Project directory

--- Portfolio Project/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    const xp = document.querySelector(".xp-fill");
-    xp.style.width = "0%";
-
-    setTimeout(() => {
-        xp.style.transition = "width 1.5s ease-out";
-        xp.style.width = "70%";
-    }, 300);
-};
